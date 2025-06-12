e725531c11fa0ef7e974d2a80892e439f32dd068
Nimmio <39748589+Nimmio@users.noreply.github.com>
2025-06-12 12:04:57 +0200

    fixed settings page

--- app/routes/_authenticated/settings.tsx
+++ app/routes/_authenticated/settings.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { ThemeModeToggle } from "@/components/theme-mode-toggle";
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_authenticated/settings")({
   component: RouteComponent,
 });
 
-async function RouteComponent() {
+function RouteComponent() {
   return (
     <>
       <div className="container mx-auto ">
